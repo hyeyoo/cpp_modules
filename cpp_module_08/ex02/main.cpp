431151f45e8d1ed8126399aed77eb516c94f450f
user42 <lmartin@student.42.fr>
2020-07-25 01:23:41 +0200

    cpp_08

--- cpp_module_08/ex02/main.cpp
+++ cpp_module_08/ex02/main.cpp
@@ -23,9 +23,9 @@ int			main(void)
 
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+	std::cout << "top: " << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	std::cout << "size: " << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
